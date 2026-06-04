47459391a15b73466104da68a7e4cf272d506d9a
kael13 <michael.rabino13@gmail.com>
2026-06-04 19:15:26 +0800

    first commit

--- hand_controller.py
+++ hand_controller.py
@@ -12,7 +12,6 @@ from config import (
     SMOOTHING_FACTOR, ONE_EURO_ENABLED, MIN_CUTOFF, BETA, DCUTOFF,
     MAX_FPS, CLICK_COOLDOWN,
     TAP_DOWN_THRESHOLD, TAP_RELEASE_THRESHOLD,
-    DEADZONE_RADIUS, DEADZONE_FRAMES,
 )
 
 pyautogui.FAILSAFE = False
@@ -48,12 +47,6 @@ class HandController:
         self._roi_min = ROI_MARGIN
         self._roi_max = 1.0 - ROI_MARGIN
         self._roi_range = 1.0 - 2 * ROI_MARGIN
-        self._deadzone_count = 0
-        self._locked_nx = 0.5
-        self._locked_ny = 0.5
-        self._tap_lock_active = False
-        self._tap_lock_nx = 0.5
-        self._tap_lock_ny = 0.5
 
     def _norm_to_screen(self, nx, ny):
         return int((nx - self._roi_min) / self._roi_range * SCREEN_WIDTH), \
@@ -67,7 +60,7 @@ class HandController:
         if dt <= 0 or dt > 0.1:
             self._filt_x, self._filt_y = raw_nx, raw_ny
             self._dx_filt, self._dy_filt = 0.0, 0.0
-            return
+            return self._norm_to_screen(raw_nx, raw_ny)
 
         if ONE_EURO_ENABLED:
             dx = (raw_nx - self._filt_x) / dt
@@ -82,32 +75,15 @@ class HandController:
             fc_y = MIN_CUTOFF + BETA * abs(self._dy_filt)
 
             alpha_x = 1.0 / (1.0 + dt * 2 * math.pi * fc_x)
-            alpha_y = 1.0 / (1.0 + dt * 2 * math.pi * fc_y)
 
+            alpha_y = 1.0 / (1.0 + dt * 2 * math.pi * fc_y)
             self._filt_x += (raw_nx - self._filt_x) * (1 - alpha_x)
             self._filt_y += (raw_ny - self._filt_y) * (1 - alpha_y)
         else:
             self._filt_x = self._filt_x * SMOOTHING_FACTOR + raw_nx * (1 - SMOOTHING_FACTOR)
             self._filt_y = self._filt_y * SMOOTHING_FACTOR + raw_ny * (1 - SMOOTHING_FACTOR)
 
-    def _apply_deadzone(self, nx, ny):
-        dist = math.hypot(nx - self._locked_nx, ny - self._locked_ny)
-        if dist < DEADZONE_RADIUS:
-            self._deadzone_count += 1
-            if self._deadzone_count >= DEADZONE_FRAMES:
-                return self._norm_to_screen(self._locked_nx, self._locked_ny)
-        else:
-            self._deadzone_count = 0
-        self._locked_nx, self._locked_ny = nx, ny
-        return self._norm_to_screen(nx, ny)
-
-    def _is_pointing(self, hand):
-        return (
-            hand.landmark[8].y < hand.landmark[6].y
-            and hand.landmark[12].y > hand.landmark[10].y
-            and hand.landmark[16].y > hand.landmark[14].y
-            and hand.landmark[20].y > hand.landmark[18].y
-        )
+        return self._norm_to_screen(self._filt_x, self._filt_y)
 
     def _detect_tap(self, y_norm):
         velocity = y_norm - self._prev_y
@@ -136,7 +112,7 @@ class HandController:
                     print("Error: Failed to capture frame.")
                     break
 
-                frame = cv2.flip(frame, 1)
+                frame = cv2.flip(frame, 1)no
                 rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 results = self.hands.process(rgb)
 
@@ -144,38 +120,13 @@ class HandController:
                     hand = results.multi_hand_landmarks[0]
                     tip = hand.landmark[8]
 
-                    if not self._is_pointing(hand):
-                        self._tap_armed = False
-                        self._tap_lock_active = False
-                        continue
-
                     nx = max(self._roi_min, min(self._roi_max, tip.x))
                     ny = max(self._roi_min, min(self._roi_max, tip.y))
-                    self._filter(nx, ny)
-
-                    tap_fired = self._detect_tap(tip.y)
-
-                    if self._tap_armed:
-                        if not self._tap_lock_active:
-                            self._tap_lock_active = True
-                            self._tap_lock_nx = self._filt_x
-                            self._tap_lock_ny = self._filt_y
-                    elif not tap_fired:
-                        self._tap_lock_active = False
-
-                    if self._tap_lock_active:
-                        lock_x, lock_y = self._apply_deadzone(
-                            self._tap_lock_nx, self._tap_lock_ny
-                        )
-                        pyautogui.moveTo(lock_x, lock_y)
-                    else:
-                        final_x, final_y = self._apply_deadzone(
-                            self._filt_x, self._filt_y
-                        )
-                        pyautogui.moveTo(final_x, final_y)
+                    final_x, final_y = self._filter(nx, ny)
+                    pyautogui.moveTo(final_x, final_y)
 
                     if (
-                        tap_fired
+                        self._detect_tap(tip.y)
                         and (time.time() - self.prev_click_time) > CLICK_COOLDOWN
                     ):
                         pyautogui.click()
